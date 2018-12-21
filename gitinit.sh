349730a303a3e46f669c5d65dbef53772776a609
BoNian-Lin <s10159023@gmail.com>
2018-12-22 02:08:30 +0800

    Fix Oh-my-zsh installation and README.md

--- gitinit.sh
+++ gitinit.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-# ********************************
-# git configuration shell script
-# ********************************
-#
+# 
 # Setup your name,email,default editor,remote repository.
 #
 
+# User Configurations
 NAME= 
 EMAIL=
 EDITOR=
 REPO=
 REPO_NAME=
+
+# Git initialization
 git init
 git config --global user.name $NAME
 git config --global user.email $EMAIL
 git config --global color.ui true
 git config --global core.editor $EDITOR
 git remote add $REPO_NAME $REPO
+echo ====== Git Configurations ======"\n"
+git config --list
